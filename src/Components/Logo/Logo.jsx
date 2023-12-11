dc344ece6a9beca8a315088b0c7c69d062995789
SheidaMohassesy <sheida.mohassesy@gmail.com>
2023-12-11 21:24:53 +0100

    Initialize the app

---
+++ src/Components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import "./Logo.css"
+
+const Logo = () => {
+	return (
+		<div>
+			<h1 className="logo">Shegraphy</h1>
+		</div>
+	)
+}
+export default Logo;
---
+++ src/Layouts/Header/Header.jsx
@@ -0,0 +1,15 @@
+import 	Logo from "../../Components/Logo/Logo";
+import "./Header.css";
+
+const Header = () => {
+	return (
+	<div className="header">
+		<Logo />
+		<a>Projects</a>
+		<a>Contact</a>
+		<a>About</a>
+	</div>
+	)
+
+}
+export default Header;
