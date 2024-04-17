1dbb253519cbc23a00f0f1db0b6cb35de4052b8e
SheidaMohassesy <sheida.mohassesy@gmail.com>
2024-04-17 14:16:09 +0200

    delete existing source code

--- src/Components/Photo/Photo.tsx
+++
@@ -1,7 +0,0 @@
-import "./Photo.css";
-
-const Photo = () => {
-  return <div className="photo-container"></div>;
-};
-
-export default Photo;
--- src/Layouts/Footer/Footer.tsx
+++
@@ -1,6 +0,0 @@
-import "./Footer.css";
-
-const Footer = () => {
-  return <div className="footer">Copyright Sheida | Test footer</div>;
-};
-export default Footer;
